03d6d317e541d68809fffe648ec375f8cb80df9b
raphael-corize <raphael.hebert@corize.co.jp>
2024-08-25 19:31:32 +0200

    added elements to form

--- src/components/RequestForm/index.tsx
+++ src/components/RequestForm/index.tsx
@@ -1,19 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { useFormik } from "formik";
 import * as yup from "yup";
-import { Button, TextField } from "@mui/material";
+import {
+  Button,
+  TextField,
+  Input,
+  InputLabel,
+  FormControlLabel,
+  Checkbox,
+  RadioGroup,
+  Radio,
+} from "@mui/material";
+
+// TODO: add condition to display taxonomy fields and show text or number input for s and x options
+// TODO: add conditionnal validation for taxonomy
+// TODO: add styling to the page, group elements
 
 const validationSchema = yup.object({
   request: yup
     .string()
     .min(8, "Request should be of minimum 8 characters length") // to be redefined "startsWith ?"
     .required("A request is required"),
+  cds: yup.string(), // to be refined with regexp?
+  file: yup.mixed().test("fileSize", "The file is too large", (value: any) => {
+    if (!value || !value.length) return true; // attachment is optional
+    return value[0].size <= 2000000;
+  }),
+  taxids: yup.boolean(),
+  gff: yup.boolean(),
+  tsv: yup.boolean(),
+  info: yup.boolean(),
 });
 
 const RequestForm: React.FC = () => {
+  const [value, setValue] = useState("none");
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue((event.target as HTMLInputElement).value);
+  };
+
   const formik = useFormik({
     initialValues: {
       request: "",
+      cds: "",
+      file: null,
+      taxids: false,
+      gff: false,
+      tsv: false,
     },
     validationSchema: validationSchema,
     onSubmit: (values) => {
@@ -38,7 +71,63 @@ const RequestForm: React.FC = () => {
           helperText={formik.touched.request && formik.errors.request}
           focused
         />
-
+        <TextField
+          fullWidth
+          id="cds"
+          name="cds"
+          label="cds"
+          value={formik.values.cds}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={formik.touched.cds && Boolean(formik.errors.cds)}
+          helperText={formik.touched.cds && formik.errors.cds}
+          focused
+        />
+        <InputLabel>List file</InputLabel>
+        <Input
+          type="file"
+          id="list"
+          name="list"
+          inputProps={{ accept: ".txt, .text" }}
+        />
+        <FormControlLabel control={<Checkbox id="taxids" />} label="taxids" />
+        <FormControlLabel control={<Checkbox id="gff" />} label="gff" />
+        <FormControlLabel control={<Checkbox id="tsv" />} label="tsv" />
+        <FormControlLabel control={<Checkbox id="info" />} label="info" />
+        <RadioGroup
+          row
+          aria-labelledby="taxonomy"
+          name="taxonomy"
+          id="taxonomy"
+          value={value}
+          onChange={handleChange}
+        >
+          <FormControlLabel
+            value="kingdom"
+            control={<Radio />}
+            label="Kingdom"
+            // disabled={value === "none"}
+          />
+          <FormControlLabel
+            value="phylum"
+            control={<Radio />}
+            label="Phylum"
+            // disabled={value === "none"}
+          />
+          <FormControlLabel
+            value="level"
+            control={<Radio />}
+            label="Level"
+            // disabled={value === "none"}
+          />
+          <FormControlLabel
+            value="custom"
+            control={<Radio />}
+            label="Custom"
+            // disabled={value === "none"}
+          />
+          <FormControlLabel value="none" control={<Radio />} label="none" />
+        </RadioGroup>
         <Button
           color="primary"
           variant="contained"
